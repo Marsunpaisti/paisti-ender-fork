123ae74b91aea13316f1c65d171b6c129dab5339
Paisti <marsunpaisti@gmail.com>
2026-04-19 02:15:06 +0300

    add more gob inspect data

--- src/haven/Gob.java
+++ src/haven/Gob.java
@@ -1147,12 +1147,31 @@ public class Gob implements RenderTree.Node, Sprite.Owner, Skeleton.ModOwner, Eq
 	String info = String.format("%s [%d]", resid(), sdt());
 	if(!full) {return info;}
 
+	java.util.List<String> lines = new ArrayList<>();
+	lines.add(info);
+	lines.add(String.format("id: %d", id));
+
+	int sdt = sdt();
+	lines.add(String.format("sdt-dec: %d", sdt));
+	lines.add(String.format("sdt-hex: 0x%s", Integer.toHexString(sdt)));
+	lines.add(String.format("sdt-bytes: %s", inspectSdtBytes()));
+	lines.add(String.format("sdt-bits: %s", inspectSdtBits(sdt)));
+
+	String attribs = inspectAttribs();
+	if(attribs != null) {
+	    lines.add(String.format("attribs: %s", attribs));
+	}
+
+	String overlays = inspectOverlays();
+	if(overlays != null) {
+	    lines.add(String.format("overlays: %s", overlays));
+	}
+
 	String mats = CustomizeVarMat.formatMaterials(this);
 	if(mats != null) {
-	    info += "\n" + mats;
-
+	    lines.add(mats);
 	}
-	return info;
+	return String.join("\n", lines);
     }
 
     public float hp() {
@@ -1501,6 +1520,95 @@ public class Gob implements RenderTree.Node, Sprite.Owner, Skeleton.ModOwner, Eq
 	    return dw.sdt.clone();
 	}
 	return null;
+    }
+
+    private String inspectSdtBytes() {
+	Message sdt = sdtm();
+	if((sdt == null) || sdt.eom()) {
+	    return "none";
+	}
+
+	byte[] data = sdt.bytes();
+	StringBuilder buf = new StringBuilder();
+	for(int i = 0; i < data.length; i++) {
+	    if(i > 0) {
+		buf.append(' ');
+	    }
+	    buf.append(String.format("%02x", data[i] & 0xff));
+	}
+	return buf.toString();
+    }
+
+    private static String inspectSdtBits(int sdt) {
+	return Integer.toBinaryString(sdt);
+    }
+
+    private String inspectAttribs() {
+	Map<Class<? extends GAttrib>, GAttrib> attrs = cloneattrs();
+	if(attrs.isEmpty()) {
+	    return "none";
+	}
+
+	java.util.List<String> names = new ArrayList<>();
+	for(GAttrib attr : attrs.values()) {
+	    if(attr != null) {
+		names.add(attr.getClass().getSimpleName());
+	    }
+	}
+	if(names.isEmpty()) {
+	    return "none";
+	}
+	Collections.sort(names);
+	return String.join(", ", names);
+    }
+
+    private String inspectOverlays() {
+	Collection<Overlay> overlays;
+	synchronized(ols) {
+	    overlays = new ArrayList<>(ols);
+	}
+
+	java.util.List<String> names = new ArrayList<>();
+	for(Overlay ol : overlays) {
+	    String name = inspectOverlay(ol);
+	    if(name != null) {
+		names.add(name);
+	    }
+	}
+	if(names.isEmpty()) {
+	    return "none";
+	}
+	Collections.sort(names);
+	return String.join(", ", names);
+    }
+
+    private String inspectOverlay(Overlay ol) {
+	if(ol == null) {
+	    return null;
+	}
+	Indir<Resource> res = null;
+	if(ol.sm instanceof OCache.OlSprite) {
+	    res = ((OCache.OlSprite)ol.sm).res;
+	} else if(ol.sm instanceof Sprite.Mill.FromRes) {
+	    res = ((Sprite.Mill.FromRes)ol.sm).res;
+	}
+	if(res != null) {
+	    try {
+		return res.get().name;
+	    } catch(Loading ignored) {
+		return "loading";
+	    }
+	}
+	if((ol.spr != null) && (ol.spr.res != null)) {
+	    return ol.spr.res.name;
+	}
+	if(ol.spr != null) {
+	    return String.format("%s#%d", ol.spr.getClass().getSimpleName(), ol.id);
+	}
+	if(ol.sm != null) {
+	    return String.format("%s#%d", ol.sm.getClass().getSimpleName(), ol.id);
+	}
+	return String.format("overlay#%d", ol.id);
     }
     
     public void setQuality(int q) {
